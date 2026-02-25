5f67383021999fc01b867c462460afb0ec57dc15
saikoro0615 <saikoro0615@gmail.com>
2026-02-26 00:59:27 +0900

    categoriesテーブルにデータの登録と削除追加

--- model.py
+++ model.py
@@ -1,10 +1,10 @@
 import sqlite3
 
 class Model:
-  def __init__(self, db_name="household.db"):
+  def __init__(self):
     #データベース作成
-    self.conn = sqlite3.connect(db_name)
-    self.cursor = self.conn.cursor()
+    self.db_name="household.db"
+    self.open_db()
     #外部キー有効化
     self.cursor.execute("PRAGMA foreign_keys= ON;")
     #categoriesテーブル作成
@@ -27,9 +27,49 @@ class Model:
         ON DELETE CASCADE
     );
     """)
-    #データベースのコミットと接続終了
+    self.close_db()
+
+  #データベースにアクセスしてカーソルを作る関数
+  def open_db(self):
+    self.conn = sqlite3.connect(self.db_name)
+    self.cursor =self.conn.cursor()
+
+  #データベースのコミットと接続終了する関数
+  def close_db(self):
     self.conn.commit()
     self.conn.close()
+
+  #categoriesテーブルにデータを登録
+  def insert_category(self, name, type):
+    self.open_db()
+    try:
+      self.cursor.execute("""INSERT INTO categories (name, type) VALUES (?, ?)""",(name, type))
+    except sqlite3.IntegrityError:
+      print("typeは'income'もしくは'expense'のみです")
+    self.close_db()
+
+  #transactionsテーブルにデータを登録
+  def insert_transactions(self, date, amount, category_name, category_type, memo):
+    pass
+
+  #categoriesテーブルからデータを削除
+  def delete_category(self, category_id):
+    self.open_db()
+    self.cursor.execute("""DELETE FROM categories WHERE id=?""",(category_id,))
+    self.close_db()
+
+  #transactionsテーブルからデータを削除
+  def delete_transactions(self, date, amount, category_name, category_type, memo):
+    pass
+
+  #指定したテーブルの表示
+  def print_db(self, table_name):
+    self.open_db()
+    self.cursor.execute(f'SELECT * FROM {table_name}')
+    print(self.cursor.fetchall())
+    self.close_db()
+
+
 
 if __name__ == "__main__":
   model = Model()
