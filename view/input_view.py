c5c7136e7b3d83a371c7ae034ca48c62f5f44fa6
saikoro0615 <saikoro0615@gmail.com>
2026-03-17 00:00:02 +0900

    main_viewに画面切り替え用の辞書を追加して、input,conf_viewの枠を作成

---
+++ view/input_view.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+class InputView(tk.Frame):
+  def __init__(self, master):
+    super().__init__(master)
+
+    #グリッド設計（4列8行）
+    for i in range(4):  #4列
+      self.grid_columnconfigure(i, weight=1)
+    for i in range(8):  #8行
+      self.grid_rowconfigure(i, weight=1)
---
+++ view/main_view.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+from view.input_view import InputView
+from view.conf_view import ConfView
+
+class MainView(tk.Tk):
+  def __init__(self):
+    super().__init__()
+
+    #ウィンドウタイトル
+    self.title("家計簿アプリ")
+    #ウィンドウの大きさ
+    self.geometry("800x600")
+    #ウィンドウのグリッドを１ｘ１にする
+    self.grid_rowconfigure(0, weight=1)
+    self.grid_columnconfigure(0, weight=1)
+    
+    #画面用のframe作成
+    container = tk.Frame(self)
+    container.grid(row=0, column=0, sticky="nsew")
+    
+    container.grid_rowconfigure(0, weight=1)
+    container.grid_columnconfigure(0, weight=1)
+
+    #画面を追加
+    self.frames = {}
+
+    self.frames["input"] = InputView(container)
+    self.frames["conf"] = ConfView(container)
+
+    #追加した画面を配置
+    for frame in self.frames.values():
+      frame.grid(row=0, column=0, sticky="nsew")
+    
+    #初期状態はinput画面を表示
+    self.show_frame("input")
+
+  
+  def show_frame(self, name):
+    self.frames[name].tkraise()
