c1079d7070f5263b99918a13dfc18cfcd67a86f4
saikoro0615 <saikoro0615@gmail.com>
2026-03-16 22:01:33 +0900

    Viewクラスのconf_form作成

--- view.py
+++ view.py
@@ -16,9 +16,9 @@ class ChangePageButtons(tk.Frame):
     tk.Button(self, text="１：書き込み").grid(row=0,column=0, sticky="nsew")
     tk.Button(self, text="２：カレンダー").grid(row=1, column=0, sticky="nsew")
 
-class DateView(tk.Frame):
+class DateTimeView(tk.Frame):
   """日付の表示とボタンで日付のプラスマイナスをするクラス"""
-  def __init__(self, master, add_command, sub_command):
+  def __init__(self, master):
     super().__init__(master)
 
     #1行6列のグリッド作成
@@ -81,6 +81,52 @@ class memoView(tk.Frame):
     self.memo_text.grid(columnspan=5,row=0,column=1, padx=10,pady=10,sticky="nsew")
 
 
+class CalenderView(tk.Frame):
+  """収支を日付ごとに表示するカレンダー用クラス"""
+  def __init__(self, master):
+    super().__init__(master)
+
+    self.grid_rowconfigure(0, weight=1)
+    self.grid_columnconfigure(0, weight=1)
+
+    #テスト用でリストで配置
+    self.calender = tk.Listbox(self)
+    self.calender.grid(row=0, column=0, sticky="nsew")
+
+
+class IncomeAndExpenseView(tk.Frame):
+  """収支を表示するラベル用クラス"""
+  def __init__(self,master):
+    super().__init__(master)
+
+    #2行1列のグリッド作成
+    for i in range(2):
+      self.grid_rowconfigure(i, weight=0)
+    for i in range(1):
+      self.grid_columnconfigure(i, weight=0)
+    
+    #ラベル
+    self.display_label = tk.Label(self, text="display", font=("Arial", 20),bd=2, relief="solid")
+    self.display_label.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
+    #収支表示ラベル
+    self.incAndExp_label = tk.Label(self, text="inc-or-exp",fg="Black",font=("Arial", 20),bd=2, relief="solid")
+    self.incAndExp_label.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
+
+
+class MounthIncAndExpListView(tk.Frame):
+  """月間の収支を日付ごとに表示するリスト用クラス"""
+  def __init__(self, master):
+    super().__init__(master)
+
+    self.grid_columnconfigure(0, weight=1)
+    self.grid_rowconfigure(0, weight=1)
+
+    #リストボックス（テスト用）
+    self.mountlist = tk.Listbox(self)
+    self.mountlist.grid(row=0, column=0, sticky="nsew")
+
+
+"""View本体"""
 class View(tk.Tk):
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
@@ -107,13 +153,13 @@ class View(tk.Tk):
     self.input_frame = tk.Frame(container)
     self.input_frame.grid(row=0, column=0, sticky="nsew")
 
-    #グリッド設計（4列12行）
+    #グリッド設計（4列8行）
     for i in range(4):  #4列
       self.input_frame.grid_columnconfigure(i, weight=1)
-    for i in range(8):  #12行
+    for i in range(8):  #8行
       self.input_frame.grid_rowconfigure(i, weight=1)
     #画面切り替え用ボタン
-    ChangePageButtons(self.input_frame).grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
+    ChangePageButtons(self.input_frame).grid(rowspan=2,row=0, column=0, padx=10, pady=10, sticky="nsew")
     #収入ボタン
     self.income_button = tk.Button(self.input_frame,text="収入",bg="lightblue", font=("Arial", 20),bd=2)
     self.income_button.grid(row=0, column=1, padx=10,pady=10,sticky="nsew")
@@ -121,8 +167,8 @@ class View(tk.Tk):
     self.expense_button = tk.Button(self.input_frame,text="支出",bg="lightblue", font=("Arial", 20),bd=2)
     self.expense_button.grid(row=0, column=2, padx=10,pady=10,sticky="nsew")
     #日付表示クラス
-    self.date_label = DateView(self.input_frame, self.add_command, self.sub_command)
-    self.date_label.grid(columnspan=2,row=1, column=1, padx=10,pady=10,sticky="nsew")
+    self.dateView_label = DateTimeView(self.input_frame)
+    self.dateView_label.grid(columnspan=2,row=1, column=1, padx=10,pady=10,sticky="nsew")
     #メモ記入ボックス
     self.memo_textbox = memoView(self.input_frame)
     self.memo_textbox.grid(columnspan=2,row=2,column=1, padx=10,pady=10,sticky="nsew")
@@ -141,26 +187,42 @@ class View(tk.Tk):
     """
     self.conf_frame = tk.Frame(container)
     self.conf_frame.grid(row=0, column=0, sticky="nsew")
-    ChangePageButtons(self.conf_frame).grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
 
-    self.income_button = tk.Button(self.conf_frame,text="収入", bg="lightblue")
-    self.income_button.grid(row=0, column=1, padx=10,pady=10,sticky="nsew")
-    
-    self.expense_button = tk.Button(self.conf_frame,text="支出", bg="lightblue")
-    self.expense_button.grid(row=0, column=2, padx=10,pady=10,sticky="nsew")
+    #グリッド設計（4列8行）
+    for i in range(4):  #4列
+      self.conf_frame.grid_columnconfigure(i, weight=1)
+    for i in range(8):  #8行
+      self.conf_frame.grid_rowconfigure(i, weight=1)
+    #画面切り替えボタン
+    ChangePageButtons(self.conf_frame).grid(rowspan=2,row=0, column=0, padx=10, pady=10, sticky="nsew")
+    #日付表示ラベル（月）
+    self.monthView_label = DateTimeView(self.conf_frame)
+    self.monthView_label.grid(columnspan=3, row=0, column=1, padx=10, pady=10, sticky="nsew")
+    #カレンダー
+    self.calender = CalenderView(self.conf_frame)
+    self.calender.grid(rowspan=3, columnspan=3, row=1, column=1, padx=10, pady=10, sticky="nsew")
 
-    self.testbutton = tk.Button(self.conf_frame,text="test").grid(row=1, column=2)
 
-    self.input_frame.tkraise()
-    # self.conf_frame.tkraise()
+    #総収入表示ラベル
+    self.totalIncome_label = IncomeAndExpenseView(self.conf_frame)
+    self.totalIncome_label.incAndExp_label.config(fg="Blue")
+    self.totalIncome_label.grid(row=4, column=1, padx=10, pady=10, sticky="nsew")
+    #総支出表示ラベル
+    self.totalExpense_label = IncomeAndExpenseView(self.conf_frame)
+    self.totalExpense_label.incAndExp_label.config(fg="Red")
+    self.totalExpense_label.grid(row=4, column=2, padx=10, pady=10, sticky="nsew")
+    #総収支表示ラベル
+    self.totalIncAndExp_label = IncomeAndExpenseView(self.conf_frame)
+    self.totalIncAndExp_label.grid(row=4, column=3, padx=10, pady=10, sticky="nsew")
+    #月間収支リスト
+    self.mounthIncAndExp_list = MounthIncAndExpListView(self.conf_frame)
+    self.mounthIncAndExp_list.grid(rowspan=3, columnspan=3, row=5, column=1, padx=10, pady=10, sticky="nsew")
 
-  def add_command(self):
-    print("add_test")
+    """デバッグ用"""
+    # self.input_frame.tkraise()
+    self.conf_frame.tkraise()
+
   
-  def sub_command(self):
-    print("sub_test")
-
-
 if __name__ == "__main__":
   view = View()
   view.mainloop()
