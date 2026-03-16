67d86ec8c3f82d8eca5ae353749ac0da9514c709
saikoro0615 <saikoro0615@gmail.com>
2026-03-16 15:26:47 +0900

    viewクラスの日付表示などをクラスに変更

--- view.py
+++ view.py
@@ -2,12 +2,12 @@ import tkinter as tk
 from tkinter import ttk
 
 
-#画面を切り替える用の共通ボタン用クラス
 class ChangePageButtons(tk.Frame):
-  def __init__(self, parent):
-    super().__init__(parent)
+  """画面を切り替える用の共通ボタン用クラス"""
+  def __init__(self, master):
+    super().__init__(master)
 
-    # 2行1列のグリッドの重み付け
+    # 2行2列のグリッドの重み付け
     self.grid_rowconfigure(0, weight=1)
     self.grid_rowconfigure(1, weight=1)
     self.grid_columnconfigure(0, weight=1)
@@ -15,6 +15,71 @@ class ChangePageButtons(tk.Frame):
 
     tk.Button(self, text="１：書き込み").grid(row=0,column=0, sticky="nsew")
     tk.Button(self, text="２：カレンダー").grid(row=1, column=0, sticky="nsew")
+
+class DateView(tk.Frame):
+  """日付の表示とボタンで日付のプラスマイナスをするクラス"""
+  def __init__(self, master, add_command, sub_command):
+    super().__init__(master)
+
+    #1行6列のグリッド作成
+    for i in range(1):#1行
+      self.grid_rowconfigure(i, weight=1)
+    for i in range(6):#6列
+      self.grid_columnconfigure(i, weight=1)
+
+    #ラベル
+    date_lebel = tk.Label(self, text="日付", font=("Arial", 20),bd=2, relief="solid")
+    date_lebel.grid(row=0, column=0, sticky="nsew")
+
+    #日付をプラスするボタン
+    add_day_button = tk.Button(self, text="▶")
+    add_day_button.grid(row=0, column=5, padx=10, pady=10, sticky="nsew")
+
+    #日付をマイナスするボタン
+    sub_day_button = tk.Button(self, text="◀")
+    sub_day_button.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")
+
+    #日付を表示するラベル
+    dateView_label = tk.Label(self, text="test", font=("Arial", 20),bd=2, relief="solid" )
+    dateView_label.grid(columnspan=3, row=0, column=2, padx=10,pady=10,sticky="nsew")
+
+class amountView(tk.Frame):
+  """金額を記入するボックス用クラス"""
+  def __init__(self, master):
+    super().__init__(master)
+
+    #1行6列のグリッド作成
+    for i in range(1):#1行
+      self.grid_rowconfigure(i, weight=1)
+    for i in range(6):#6列
+      self.grid_columnconfigure(i, weight=1)
+    
+    #ラベル
+    amount_label = tk.Label(self, text="金額", font=("Arial", 20),bd=2, relief="solid")
+    amount_label.grid(row=0, column=0, sticky="nsew")
+
+    #テキストボックス
+    amount_text = tk.Entry(self)
+    amount_text.grid(columnspan=5, row=0, column=1, padx=10,pady=10,sticky="nsew")
+
+class memoView(tk.Frame):
+  """メモを記入するボックス用クラス"""
+  def __init__(self, master):
+    super().__init__(master)
+
+    #1行6列のグリッド作成
+    for i in range(1):#1行
+      self.grid_rowconfigure(i, weight=1)
+    for i in range(6):#6列
+      self.grid_columnconfigure(i, weight=1)
+
+    #ラベル
+    memo_label = tk.Label(self, text="メモ", font=("Arial", 20),bd=2, relief="solid")
+    memo_label.grid(row=0, column=0, sticky="nsew")
+    #メモ記入用のテキストボックス
+    self.memo_text = tk.Entry(self)
+    self.memo_text.grid(columnspan=5,row=0,column=1, padx=10,pady=10,sticky="nsew")
+
 
 class View(tk.Tk):
   def __init__(self, *args, **kwargs):
@@ -50,25 +115,25 @@ class View(tk.Tk):
     #画面切り替え用ボタン
     ChangePageButtons(self.input_frame).grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
     #収入ボタン
-    self.income_button = tk.Button(self.input_frame,text="収入",bg="lightblue")
+    self.income_button = tk.Button(self.input_frame,text="収入",bg="lightblue", font=("Arial", 20),bd=2)
     self.income_button.grid(row=0, column=1, padx=10,pady=10,sticky="nsew")
     #支出ボタン
-    self.expense_button = tk.Button(self.input_frame,text="支出",bg="lightblue")
+    self.expense_button = tk.Button(self.input_frame,text="支出",bg="lightblue", font=("Arial", 20),bd=2)
     self.expense_button.grid(row=0, column=2, padx=10,pady=10,sticky="nsew")
-    #日付表示ラベル
-    self.date_label = tk.Label(self.input_frame, text="test", font=("Arial", 20),bd=2, relief="solid" )
+    #日付表示クラス
+    self.date_label = DateView(self.input_frame, self.add_command, self.sub_command)
     self.date_label.grid(columnspan=2,row=1, column=1, padx=10,pady=10,sticky="nsew")
     #メモ記入ボックス
-    self.memo_text = tk.Entry(self.input_frame)
-    self.memo_text.grid(columnspan=2,row=2,column=1, padx=10,pady=10,sticky="nsew")
+    self.memo_textbox = memoView(self.input_frame)
+    self.memo_textbox.grid(columnspan=2,row=2,column=1, padx=10,pady=10,sticky="nsew")
     #金額記入ボックス
-    self.amount_text = tk.Entry(self.input_frame)
-    self.amount_text.grid(columnspan=2, row=3, column=1, padx=10,pady=10,sticky="nsew")
+    self.amount_textbox = amountView(self.input_frame)
+    self.amount_textbox.grid(columnspan=2, row=3, column=1, padx=10,pady=10,sticky="nsew")
     #カテゴリリストボックス
     self.category_listbox = tk.Listbox(self.input_frame)
     self.category_listbox.grid(rowspan=3,columnspan=2,row=4,column=1,padx=10,pady=10,sticky="nsew")
     #データ登録ボタン
-    self.data_regist_buttons = tk.Button(self.input_frame, text="データ登録",bg="lightblue")
+    self.data_regist_buttons = tk.Button(self.input_frame, text="データ登録",bg="lightblue",font=("Arial", 20),bd=2)
     self.data_regist_buttons.grid(columnspan=2,row=7,column=1,padx=10,pady=10,sticky="nsew")
 
     """
@@ -88,6 +153,12 @@ class View(tk.Tk):
 
     self.input_frame.tkraise()
     # self.conf_frame.tkraise()
+
+  def add_command(self):
+    print("add_test")
+  
+  def sub_command(self):
+    print("sub_test")
 
 
 if __name__ == "__main__":
